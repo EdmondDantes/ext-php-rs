f43ed43901313e47a74dff11e1152df131b3eb4f
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-12-02 21:07:42 +0200

    + setup_function_hooks and remove_function_hooks

--- src/hooks/function_hooks.rs
+++ src/hooks/function_hooks.rs
@@ -1,6 +1,7 @@
 use std::cell::RefCell;
 use std::ffi::c_void;
 use crate::{types, zend};
+use crate::zend::{Function, ExecutorGlobals};
 
 pub struct ZendFunctionHook {
     pub hooked_function_name: String,
@@ -40,4 +41,41 @@ pub fn remove_all_function_hooks() {
     FUNCTION_HOOKS.with(|hooks| {
         hooks.borrow_mut().clear();
     });
+}
+
+pub fn setup_function_hooks() {
+    FUNCTION_HOOKS.with(|hooks| {
+        for hook in hooks.borrow().iter() {
+            hook_function(hook.handler as *const c_void (), &hook.hooked_function_name).unwrap();
+        }
+    });
+}
+
+pub fn remove_function_hooks() {
+    FUNCTION_HOOKS.with(|hooks| {
+        for hook in hooks.borrow().iter() {
+            hook_function(hook.previous_handler.unwrap(), &hook.hooked_function_name).unwrap();
+        }
+    });
+}
+
+fn hook_function(func_ptr: *const c_void (), func_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    // Получаем глобальную таблицу функций
+    let function_table = ExecutorGlobals::get()
+        .function_table()
+        .expect("Error: function table is not available");
+
+    if let Some(zval_function) = function_table.get(func_name) {
+        let mut zend_function = Function::from(zval_function.as_ptr());
+
+        unsafe {
+            zend_function.internal_function.handler = Some(func_ptr);
+        }
+
+    } else {
+        return Err(print!("The function '{}' was not found in Zend.", func_name).into());
+    }
+
+    Ok(())
 }
